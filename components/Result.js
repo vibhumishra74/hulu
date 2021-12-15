137f9394f92f1305530827a540818f843069a261
vibhu <vibhumishra74@gmail.com>
2021-12-15 16:01:31 +0530

    complet

--- components/Result.js
+++ components/Result.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import Thumbnail from './Thumbnail'
+import FlipMove from 'react-flip-move'
 function Result({request}) {
+    // console.log('object request',request)
     return (
-        <div>
-            <p>hello result from result</p>
-        </div>
+        <FlipMove className='mt-10 px-5 sm:grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 3xl:flex flex-wrap justify-center'>
+           {request.map(result=>(
+               <Thumbnail key={result.id} result={result} /> //inflip move child component must have key props
+           ))}
+        </FlipMove>
     )
 }
 
